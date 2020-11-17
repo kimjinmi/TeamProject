c0b132a65bf5ce6b57af2ac6bfbea147e21e3bde
COM <COM@DESKTOP-TH36TT5>
2020-11-17 22:34:09 +0900

    1034

--- src/main/java/com/mycompany/webapp/controller/aboutController.java
+++ src/main/java/com/mycompany/webapp/controller/aboutController.java
@@ -2,15 +2,17 @@ package com.mycompany.webapp.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/about")
+@Controller
+@RequestMapping("/aboutus")
 public class aboutController {
 	private static final Logger logger = LoggerFactory.getLogger(aboutController.class);
 
-	@RequestMapping("/content")
-	public String content() { //http://localhost:8080/teamproject
+	@RequestMapping("/about")
+	public String about() { //http://localhost:8080/teamproject
 		logger.info("실행");
-		return "home";
+		return "aboutus/about";
 	}
 }
