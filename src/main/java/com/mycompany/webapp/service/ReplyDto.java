5cb102c8e920b625c75e8d039b89172a42c91ab2
COM <COM@DESKTOP-TH36TT5>
2020-11-20 16:05:18 +0900

    영아 replyDto추가

---
+++ src/main/java/com/mycompany/webapp/service/ReplyDto.java
@@ -0,0 +1,42 @@
+package com.mycompany.webapp.service;
+
+public class ReplyDto {
+
+	private	int rno;
+	private String rcontent;
+	private String firstreply;
+	private String memail;
+	private int bno;
+	
+	public int getRno() {
+		return rno;
+	}
+	public void setRno(int rno) {
+		this.rno = rno;
+	}
+	public String getRcontent() {
+		return rcontent;
+	}
+	public void setRcontent(String rcontent) {
+		this.rcontent = rcontent;
+	}
+	public String getFirstreply() {
+		return firstreply;
+	}
+	public void setFirstreply(String firstreply) {
+		this.firstreply = firstreply;
+	}
+	public String getMemail() {
+		return memail;
+	}
+	public void setMemail(String memail) {
+		this.memail = memail;
+	}
+	public int getBno() {
+		return bno;
+	}
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+	
+}
