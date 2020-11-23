8f7459ef387ca5863682e80b053d75b115f29184
JIHOON KIM <listerine3949@gmail.com>
2020-11-23 12:22:03 +0900

    김지훈 commit

--- src/main/java/com/mycompany/webapp/controller/HomeController.java
+++ src/main/java/com/mycompany/webapp/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.mycompany.webapp.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -11,8 +13,9 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	@RequestMapping("/")
-	public String content() { //http://localhost:8080/teamproject
-		logger.info("실행");
+	public String content(HttpSession session) { //http://localhost:8080/teamproject
+		session.setAttribute("UserId", "123@jihoonnara.com");
+		logger.info("메인페이지 로그인");
 		return "home";
 	}
 	
@@ -20,6 +23,12 @@ public class HomeController {
 	public String elements() { //http://localhost:8080/teamproject
 		logger.info("실행");
 		return "elements";
+	}
+	
+	@RequestMapping("/login")
+	public String login() {
+		
+		return "home";
 	}
 	
 }
