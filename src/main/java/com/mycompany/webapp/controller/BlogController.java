247a80616ac25bc47756fa739c019998c4e585c7
COM <COM@DESKTOP-TH36TT5>
2020-11-27 15:55:54 +0900

    영아 - 정렬함

--- src/main/java/com/mycompany/webapp/controller/BlogController.java
+++ src/main/java/com/mycompany/webapp/controller/BlogController.java
@@ -1,12 +1,17 @@
 package com.mycompany.webapp.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -17,6 +22,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -185,20 +191,48 @@ public class BlogController {
 			 */
 		  
 		  service.commentWrite(reply);
-		  
-		  
 		  }
-		 
-		
-		int bbno = reply.getBno();
-		List<ReplyDto> commentlist = service.commentList(bbno);
-		logger.info("getBno = " + bbno);
-		logger.info("commentlist 값 = " + commentlist.toString());
-		model.addAttribute("commentlist", commentlist);
-		
-		
-
-		return "blog/blogcommentList";
+		  
+			int bbno = reply.getBno();
+			List<ReplyDto> commentlist = service.commentList(bbno);
+			logger.info("getBno = " + bbno);
+			logger.info("commentlist 값 = " + commentlist.toString());
+			model.addAttribute("commentlist", commentlist);
+			return "blog/blogcommentList";
 
 	}
+	
+	@GetMapping("/photodownload")
+	public void photodownload(String fileName, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		logger.info(fileName);
+		
+		//파일의 데이터를 읽기 위한 입력 스트림 얻기
+		String saveFilePath = "C:/temp/projectimage/member/" + fileName;
+		InputStream is = new FileInputStream(saveFilePath);
+		
+		//응답 HTTP 헤더 구성
+		//1) Content-Type 헤더 구성(파일 종류)
+		ServletContext application = request.getServletContext();
+		String fileType = application.getMimeType(fileName);
+		response.setContentType(fileType);
+		
+		//다운로드할 실제 파일 이름 구성
+		//split메소드는 배열로 리턴됨
+
+		//attachment; : 브라우저가 해당 파일을 다운로드(없으면 보여줄 수 있으면 브라우저에서 보여줌, 보여줄 수 없으면 다운로드
+		response.setHeader("Content-Disposition", "attachment; filename=\""+fileName+"\"");
+		
+		//3)Content-Length 헤더 구성(다운로드할 파일의 크기를 지정)
+		int fileSize = (int) new File(saveFilePath).length();
+		response.setContentLength(fileSize);
+		
+		//응답 HTTP의 바디(본문) 구성
+		OutputStream os = response.getOutputStream();
+		FileCopyUtils.copy(is, os); //스프링에서 제공
+		os.flush();
+		os.close();
+		is.close();
+	}
+	
+	
 }
