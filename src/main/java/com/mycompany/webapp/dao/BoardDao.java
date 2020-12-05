1c0943f7555e8d416d320eabca80b6a5900a359a
JIHOON KIM <listerine3949@gmail.com>
2020-12-06 02:51:06 +0900

    김지훈: 게시물 링크 http 있든없든 쌉가능

--- src/main/java/com/mycompany/webapp/dao/BoardDao.java
+++ src/main/java/com/mycompany/webapp/dao/BoardDao.java
@@ -22,6 +22,11 @@ public class BoardDao {
 	
 	public BoardDto selectByBno(int bno) {
 		BoardDto board = sst.selectOne("mybatis.mapper.board.selectByBno",bno);
+		String str = board.getBlinkcontent();
+		if(str.contains("http")) {
+			String[] cut = str.split("//");
+			board.setBlinkcontent(cut[1]);
+		}
 		return board;
 	}
 	
@@ -33,7 +38,7 @@ public class BoardDao {
 		Map map = new HashMap();
 		map.put("cno", cno);
 		map.put("murl", murl);
-		List<BoardDto> list = sst.selectList("mybatis.mapper.board.categoryListLink", map);
+		List<BoardDto> list = sst.selectList("mybatis.mapper.board.categoryListLink", map);	
 			return list;
 	}
 	
