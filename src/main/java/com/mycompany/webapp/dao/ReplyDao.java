55b8ee6577f34b9026f2cb3d8c6930aabbf71566
2SunE <tjsrhkdjaak@gmail.com>
2021-01-05 06:16:53 +0900

    Update ReplyDao.java

--- src/main/java/com/mycompany/webapp/dao/ReplyDao.java
+++ src/main/java/com/mycompany/webapp/dao/ReplyDao.java
@@ -7,13 +7,12 @@ import javax.annotation.Resource;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
-import com.mycompany.webapp.dto.BoardDto;
 import com.mycompany.webapp.dto.PagerDto;
 import com.mycompany.webapp.dto.ReplyDto;
 
 @Repository
 public class ReplyDao {
-	
+
 	@Resource
 	private SqlSessionTemplate sst;
 
@@ -37,7 +36,7 @@ public class ReplyDao {
 		return list;
 	}
 
-	//진미 : 전체 글의 구하는거임 삭제 ㄴ, 고쳐서 사용 금지
+	// 진미 : 전체 글의 구하는거임 삭제 ㄴ, 고쳐서 사용 금지
 	public List<ReplyDto> selectByAllPage(PagerDto pager) {
 		List<ReplyDto> list = sst.selectList("mybatis.mapper.reply.selectListAll", pager);
 		return list;
@@ -46,21 +45,14 @@ public class ReplyDao {
 	public int deleteByRno(int rno) {
 		int rows = sst.delete("mybatis.mapper.reply.deleteByRno", rno);
 		return rows;
-		
 	}
 
 	public void addComent(int rno) {
 		sst.insert("mybatis.mapper.reply.addComent", rno);
-		
 	}
 
 	public int myReplyCount(String memail) {
 		int rows = sst.selectOne("mybatis.mapper.reply.myReplyCount", memail);
 		return rows;
 	}
-
-
-	
-	
-
 }
