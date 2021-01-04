f163024da746a86d783363cca351c30a9e6c32c0
2SunE <tjsrhkdjaak@gmail.com>
2021-01-05 06:14:04 +0900

    Update DisabledDao.java

--- src/main/java/com/mycompany/webapp/dao/DisabledDao.java
+++ src/main/java/com/mycompany/webapp/dao/DisabledDao.java
@@ -1,34 +1,28 @@
 package com.mycompany.webapp.dao;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import javax.annotation.Resource;
-
 import org.mybatis.spring.SqlSessionTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
-
 import com.mycompany.webapp.dto.DisabledDto;
 import com.mycompany.webapp.dto.PagerDto;
 
 @Repository
 public class DisabledDao {
 	private static final Logger logger = LoggerFactory.getLogger(DisabledDao.class);
+	
 	@Resource
 	private SqlSessionTemplate sst;
 
-	//------------------------------------------영아 - 시작----------------------------------------------
-
-					//페이저 행의 수(비활성화 회원들)
+	//페이저 행의 수(비활성화 회원들)
 	public int mdisabledThing() {
 		int totalRows = sst.selectOne("mybatis.mapper.disabled.mdisabledThing");
 		return totalRows;
 	}
 
-					//활성화 회원들 페이지 만들기
+	//활성화 회원들 페이지 만들기
 	public List<DisabledDto> DisabledPageList(PagerDto pager) {
 		List<DisabledDto> list = sst.selectList("mybatis.mapper.disabled.DisabledPageList", pager);
 		return list;
@@ -42,7 +36,4 @@ public class DisabledDao {
 		sst.selectOne("mybatis.mapper.disabled.selectAbled", abled);
 		
 	}
-
-	//------------------------------------------영아 - 끝----------------------------------------------
-
 }
