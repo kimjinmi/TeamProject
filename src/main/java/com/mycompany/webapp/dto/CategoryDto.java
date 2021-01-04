999f4e6fa423f2ef1ab637fab9ec8ecc64d727f9
2SunE <tjsrhkdjaak@gmail.com>
2021-01-05 06:18:34 +0900

    Update CategoryDto.java

--- src/main/java/com/mycompany/webapp/dto/CategoryDto.java
+++ src/main/java/com/mycompany/webapp/dto/CategoryDto.java
@@ -5,32 +5,36 @@ public class CategoryDto {
 	private String ccontent; // Category 내용
 	private String murl;
 	private int ccount;
-	
+
 	public int getCno() {
 		return cno;
 	}
+
 	public void setCno(int cno) {
 		this.cno = cno;
 	}
+
 	public String getCcontent() {
 		return ccontent;
 	}
+
 	public void setCcontent(String ccontent) {
 		this.ccontent = ccontent;
 	}
+
 	public String getMurl() {
 		return murl;
 	}
+
 	public void setMurl(String murl) {
 		this.murl = murl;
 	}
+
 	public int getCcount() {
 		return ccount;
 	}
+
 	public void setCcount(int ccount) {
 		this.ccount = ccount;
 	}
-
-	
-	
 }
