db900a6f8f1911922444a1208059d56b51635c8f
2SunE <tjsrhkdjaak@gmail.com>
2021-01-05 06:13:24 +0900

    Update CategoryDao.java

--- src/main/java/com/mycompany/webapp/dao/CategoryDao.java
+++ src/main/java/com/mycompany/webapp/dao/CategoryDao.java
@@ -12,43 +12,43 @@ import com.mycompany.webapp.dto.CategoryDto;
 
 @Repository
 public class CategoryDao {
-	
+
 	@Resource
 	private SqlSessionTemplate sst;
-	
-	public List<CategoryDto> selectAll() {				//selectAll : Service가 호출함
-		List<CategoryDto> list = sst.selectList("mybatis.mapper.category.selectAll");		//id를 찾아서 모두를 실행해라
+
+	public List<CategoryDto> selectAll() { 
+		List<CategoryDto> list = sst.selectList("mybatis.mapper.category.selectAll"); 
 		return list;
 	}
-	
-	public List<CategoryDto> selectMurl(String murl) {				
-		List<CategoryDto> list = sst.selectList("mybatis.mapper.category.selectMurl", murl);		
+
+	public List<CategoryDto> selectMurl(String murl) {
+		List<CategoryDto> list = sst.selectList("mybatis.mapper.category.selectMurl", murl);
 		return list;
 	}
-	
-	public List<CategoryDto> selectAllCount() {				
-		List<CategoryDto> list = sst.selectList("mybatis.mapper.category.selectAllCount");		
+
+	public List<CategoryDto> selectAllCount() {
+		List<CategoryDto> list = sst.selectList("mybatis.mapper.category.selectAllCount");
 		return list;
 	}
 
 	public int insert(String addCategory) {
 		int row = sst.insert("mybatis.mapper.category.insert", addCategory);
 		return row;
-		
+
 	}
 
 	public int delete(int cno) {
 		int row = sst.insert("mybatis.mapper.category.delete", cno);
 		return row;
-		
+
 	}
 
 	public void update(int cno, String editCategory2) {
 		Map map = new HashMap();
 		map.put("cno", cno);
 		map.put("editccontent", editCategory2);
-		int row =  sst.update("mybatis.mapper.category.update", map);
-		
+		int row = sst.update("mybatis.mapper.category.update", map);
+
 	}
 
 	/*public List<CategoryDto> selectbyCno(int cno) {
