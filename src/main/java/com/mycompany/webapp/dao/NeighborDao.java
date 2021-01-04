6b943309988068250b0f082d4e318bde97f13854
2SunE <tjsrhkdjaak@gmail.com>
2021-01-05 06:16:26 +0900

    Update NeighborDao.java

--- src/main/java/com/mycompany/webapp/dao/NeighborDao.java
+++ src/main/java/com/mycompany/webapp/dao/NeighborDao.java
@@ -9,7 +9,6 @@ import javax.annotation.Resource;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
-import com.mycompany.webapp.dto.BoardDto;
 import com.mycompany.webapp.dto.NeighborDto;
 import com.mycompany.webapp.dto.PagerDto;
 
@@ -18,38 +17,31 @@ public class NeighborDao {
 
 	@Resource
 	private SqlSessionTemplate sst;
-	
-	//-------------------------------------------------영아---------------------------------------------------//
-	
-	
-				//내 이웃 리스트
+
+	// 내 이웃 리스트
 	public List<NeighborDto> selectNlistAll(String mymemail) {
-		 List<NeighborDto> list = sst.selectList("mybatis.mapper.neighbor.selectNlistAll", mymemail);
-		 return list;
+		List<NeighborDto> list = sst.selectList("mybatis.mapper.neighbor.selectNlistAll", mymemail);
+		return list;
 	}
 
-	
-				//내 이웃 삭제
+	// 내 이웃 삭제
 	public int deleteByNno(int nno) {
-		int rows = sst.delete("mybatis.mapper.neighbor.deleteByNno", nno);	
+		int rows = sst.delete("mybatis.mapper.neighbor.deleteByNno", nno);
 		return rows;
 	}
-	
-				//내 이웃 리스트 페이지 작성 과정1 - 내 이웃의 수 
+
+	// 내 이웃 리스트 페이지 작성 과정1 - 내 이웃의 수
 	public int myNeighborCount(String mymemail) {
 		int totalRows = sst.selectOne("mybatis.mapper.neighbor.myNeighborCount", mymemail);
 		return totalRows;
 	}
-	
-				//내 이웃 리스트 페이지 작성 과정 2 - 페이저
+
+	// 내 이웃 리스트 페이지 작성 과정 2 - 페이저
 	public List<NeighborDto> selectByPageNeList(PagerDto pager) {
 		List<NeighborDto> list = sst.selectList("mybatis.mapper.neighbor.selectByPageNeList", pager);
 		return list;
 	}
 
-	//-------------------------------------------------영아---------------------------------------------------//
-	
-	//진미
 	public int neighorexist(String userUrl, String memail) {
 		Map map = new HashMap();
 		map.put("userUrl", userUrl);
@@ -58,12 +50,8 @@ public class NeighborDao {
 		return existRows;
 	}
 
-
 	public int insertneighbor(NeighborDto neighbor) {
 		int rows = sst.insert("mybatis.mapper.neighbor.insert", neighbor);
 		return rows;
 	}
-
-
-	
 }
